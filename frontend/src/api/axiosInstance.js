73fe82d0e644843d6cbe7b71fc208fad9c310ed0
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-08-13 01:45:11 +0530

    Final update

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api", 
+  baseURL: "https://full-stack-developer-assessment-anuradha-8nl5.onrender.com/api", 
 });
 
 axiosInstance.interceptors.request.use((config) => {
