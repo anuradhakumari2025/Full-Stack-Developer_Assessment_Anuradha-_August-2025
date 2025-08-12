01315743d3b486e7f9d9846fec9a0f64e4a6f244
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-08-13 00:05:01 +0530

    Added authentication and middleware in backend

--- backend/src/routes/driver.routes.js
+++ backend/src/routes/driver.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const driverController = require("../controllers/driver.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
-router.get("/get", driverController.getDrivers);
-router.get("/get/:id", driverController.getDriver);
-router.post("/create", driverController.createDriver);
-router.put("/update/:id", driverController.updateDriver);
-router.delete("/delete/:id", driverController.deleteDriver);
+router.get("/get",authMiddleware, driverController.getDrivers);
+router.get("/get/:id",authMiddleware, driverController.getDriver);
+router.post("/create",authMiddleware, driverController.createDriver);
+router.put("/update/:id",authMiddleware, driverController.updateDriver);
+router.delete("/delete/:id",authMiddleware, driverController.deleteDriver);
 
 module.exports = router;
--- backend/src/routes/simulation.routes.js
+++ backend/src/routes/simulation.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const simulationController = require("../controllers/simulation.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
-router.post("/", simulationController.runSimulation);
-router.get("/latest", simulationController.getLatestSimulation); // Endpoint to get latest simulation result
+router.post("/", authMiddleware, simulationController.runSimulation);
+router.get("/latest", authMiddleware, simulationController.getLatestSimulation); // Endpoint to get latest simulation result
 
 module.exports = router;
